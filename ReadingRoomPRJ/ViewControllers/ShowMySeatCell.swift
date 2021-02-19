acad96449311f3a7ebb2d98e49dee1b61fcaddd5
Guboneui <starku2249@naver.com>
2021-02-20 06:19:44 +0900

    cell.swift 추가

---
+++ ReadingRoomPRJ/ViewControllers/ShowMySeatCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShowMySeatCell.swift
+//  ReadingRoomPRJ
+//
+//  Created by 구본의 on 2021/02/20.
+//
+
+import UIKit
+
+class ShowMySeatCell: UICollectionViewCell {
+    //cell 에 대한 값 추후 수정을 위해 swift 파일 생성해놓음
+    
+}
