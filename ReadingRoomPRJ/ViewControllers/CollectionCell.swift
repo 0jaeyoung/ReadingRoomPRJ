dc83a25efb0bfd21c034ececb4acb2983245369b
Guboneui <starku2249@naver.com>
2021-05-09 04:49:21 +0900

    좌석 전까지 수정 내용. (이전 push 주석처리 수정)

--- ReadingRoomPRJ/ViewControllers/CollectionCell.swift
+++ ReadingRoomPRJ/ViewControllers/CollectionCell.swift
@@ -45,15 +45,15 @@ class CollectionCell: UICollectionViewCell {
 
 
                   //오류 발생
-//                } else if CollectionCell.checkArr[CollectionCell.userSeatInfo] == 1 && CollectionCell.checkArr[btnNum!] == 0 && myImageView.image == UIImage(named: "emptySeat.png") && CollectionCell.countOne == true {
-//
-//                    let alert = UIAlertController(title: "경고", message: "하나의 좌석만 선택할 수 있습니다.", preferredStyle: UIAlertController.Style.alert)
-//                    let okButton = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
-//                    alert.view.tintColor = UIColor.appColor(.coal)
-//                    alert.addAction(okButton)
-//                    viewController?.present(alert, animated: false, completion: nil)
-//                }
-                }else if CollectionCell.checkArr[btnNum!] == 1 && myImageView.image == UIImage(named: "selectedSeat.png") && CollectionCell.countOne == true {
+                } else if CollectionCell.checkArr[CollectionCell.userSeatInfo] == 1 && CollectionCell.checkArr[btnNum!] == 0 && myImageView.image == UIImage(named: "emptySeat.png") && CollectionCell.countOne == true {
+
+                    let alert = UIAlertController(title: "경고", message: "하나의 좌석만 선택할 수 있습니다.", preferredStyle: UIAlertController.Style.alert)
+                    let okButton = UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil)
+                    alert.view.tintColor = UIColor.appColor(.coal)
+                    alert.addAction(okButton)
+                    viewController?.present(alert, animated: false, completion: nil)
+                }
+                else if CollectionCell.checkArr[btnNum!] == 1 && myImageView.image == UIImage(named: "selectedSeat.png") && CollectionCell.countOne == true {
 
                     CollectionCell.countOne = false
                     CollectionCell.checkArr[btnNum!] = 0
